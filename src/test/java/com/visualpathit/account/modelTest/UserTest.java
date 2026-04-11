7649c2c5b8ba1bc12600072abc9499756b8e94bc
hkhcoder <137309866+hkhcoder@users.noreply.github.com>
2026-04-11 12:58:31 +0530

    Refactor UserTest with new test structure and assertions

--- src/test/java/com/visualpathit/account/modelTest/UserTest.java
+++ src/test/java/com/visualpathit/account/modelTest/UserTest.java
@@ -1,32 +1,56 @@
 package com.visualpathit.account.modelTest;
 
+import junit.framework.Assert;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.visualpathit.account.model.Role;
 import com.visualpathit.account.model.User;
-import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+/** {@author imrant} !*/
 public class UserTest {
+	
+	public static final Long EXPECTED_ID = 1L;
+	public static final int EXPECTED_SIZE = 1;
+    public static final String EXPECTED_USERNAME = "Wahidkhan74";
+    public static final String EXPECTED_PASSWD = "Wahidkhan74";
+    public static final String EXPECTED_USEREMAIL = "XXXXX@gmail.com";
+    private User user;
+    @Before
+    public void setUp() throws Exception {
+    	   	
+    	Role role = new Role();
+    	role.setId(1L);
+        role.setName("Admin");
+        Set<Role> roles = new HashSet<Role>();    	
+        roles.add(role); 
+    	
+        user = new User();
+        user.setId(1L);
+        user.setUsername("Wahidkhan74");
+        user.setPassword("Wahidkhan74");
+        user.setUserEmail("XXXXX@gmail.com");
+        user.setRoles(roles);
+    }
 
-    private static final String TEST_USERNAME = "testUser";
-    private static final String TEST_PASSWORD = "dummyPass";
+    @After
+    public void tearDown() throws Exception {
+        System.out.println("Test Completed");
 
-    @Test
-    public void testUserCreation() {
-        User user = new User(TEST_USERNAME, TEST_PASSWORD);
-
-        assertNotNull(user);
-        assertEquals(TEST_USERNAME, user.getUsername());
-        assertEquals(TEST_PASSWORD, user.getPassword());
     }
 
     @Test
-    public void testUserSetters() {
-        User user = new User();
+    public void testUserDetailsHappyFlow() throws Exception {
+    	Assert.assertEquals(EXPECTED_ID, user.getId());
+        Assert.assertEquals(EXPECTED_USERNAME, user.getUsername());
+        Assert.assertEquals(EXPECTED_PASSWD, user.getPassword());
+        Assert.assertEquals(EXPECTED_USEREMAIL, user.getUserEmail());
+        Assert.assertEquals(EXPECTED_SIZE,user.getRoles().size());
 
-        user.setUsername(TEST_USERNAME);
-        user.setPassword(TEST_PASSWORD);
-
-        assertEquals(TEST_USERNAME, user.getUsername());
-        assertEquals(TEST_PASSWORD, user.getPassword());
     }
 }
